8da007aafb6ecf0f56debf6f3c7c64c15c6e6678
Dasha <d.bachu7@yandex.ru>
2024-12-23 17:27:47 +0300

    Lab 2 fix task2: add getter and setter

--- src/lab2/Book.java
+++ src/lab2/Book.java
@@ -17,6 +17,46 @@ public class Book {
         this.year = year;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    public void setPageCount(int pageCount) {
+        this.pageCount = pageCount;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public int getPageCount() {
+        return pageCount;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
     @Override
     public String toString() {
         return "Book {" +
